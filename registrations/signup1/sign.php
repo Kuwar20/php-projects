c7319b461eb79e519930e78f29424758be7c2b22
Kuwar20 <kuwarx1@gmail.com>
2024-05-20 15:02:09 +0530

    feat: Update signup page with form fields and submit button

--- registrations/signup1/sign.php
+++ registrations/signup1/sign.php
@@ -6,8 +6,22 @@
     <title>Signup Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+        <body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
+    <h1 class="text-center">Sign up Page</h1>
+    <div class="container mt-5">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" name="username" aria-describedby="emailHelp" placeholder="Enter your Email">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" placeholder="Enter your password" name="password">
+  </div>
+  <button type="submit" class="btn btn-primary w-100">Submit</button>
+</form>
+    </div>
+</body>
 </html>
