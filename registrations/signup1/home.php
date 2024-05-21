9769e90f544ee5697c4ee10a284f7d9504a0406b
Kuwar20 <kuwarx1@gmail.com>
2024-05-21 07:56:41 +0530

    Update signup page with user authentication and session management

---
+++ registrations/signup1/home.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['username'])){
+        header("location: login.php");
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Home</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    </head>
+    <body>
+        <h1 class='text-center text-warning mt-5'>Welcome
+        <?php
+            echo $_SESSION['username'];
+        ?></h1>
+    </body>
+</html>
