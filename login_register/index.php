31d4b697b22127a8d6aebdc5195ba6bc6dbde724
Kuwar20 <kuwarx1@gmail.com>
2024-05-22 15:31:23 +0530

    Update login and register page with header and navigation

--- login_register/index.php
+++ login_register/index.php
@@ -4,8 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login and Register</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <header>
+        <h2>Kuwar Singh</h2>
+        <nav>
+            <a href="#">HOME</a>
+            <a href="#">BLOG</a>
+            <a href="#">CONTACT</a>
+            <a href="#">ABOUT</a>
+        </nav>
+        <div class="sign-in-up">
+            <button type="button">LOGIN</button>
+            <button type="button">REGISTER</button>
+        </div>
+    </header>
 </body>
 </html>
