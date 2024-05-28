9b897c74cdb6f6d01e96e8c868e356a29dff61de
Kuwar20 <kuwarx1@gmail.com>
2024-05-28 14:24:25 +0530

    backend init: Made DB, and make utils

---
+++ secure_auth_system/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASE_URL', 'http://localhost/8001/');
+
+// Database configuration
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'secure_auth_system');
+
---
+++ secure_auth_system/utils.php
@@ -0,0 +1,42 @@
+<?php
+
+require_once 'config.php';
+
+class Utils {
+
+    // Method to sanitize input value
+    public static function sanitize($input){
+        $input = trim($input);
+        $input = stripslashes($input);
+        $input = htmlspecialchars($input);
+        return $input;
+    }
+
+    // Method to redirect to a given page
+    public static function redirect($page){
+        header('location:' . BASE_URL . '/' . $page);
+    }
+
+    // Method to set a flash message
+    public static function setFlash($name,$message){
+        if(!empty($_SESSION[$name])){
+            unset($_SESSION[$name]);
+        }
+        $_SESSION[$name] = $message;
+    }
+    // Message to display flash message
+    public static function displayFlash($name){
+        if(isset($_SESSION[$name])){
+            echo '<div class="alert alert-'. $type .'">' . $_SESSION[$name] . '</div>';
+            unset($_SESSION[$name]);
+        }
+    }
+    // Method to check if user is logged in
+    public static function isLoggedIn(){
+        if(isset($_SESSION['user'])){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
