59c5d03815e9f4e8166bd4425370e7aa814e7297
Kuwar20 <kuwarx1@gmail.com>
2024-05-29 14:46:43 +0530

    chore: Add user profile page with Bootstrap styling and user information display

---
+++ secure_auth_system/profile.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+    require_once 'utils.php';
+
+    $user = null;
+    if(isset($_SESSION['user'])){
+        $user = $_SESSION['user'];
+    }
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>User Profile</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body class='bg-dark bg-gradient'>
+    <div class="container">
+      <div class="row min-vh-100 justify-content-center align-items-center">
+        <div class="col-md-6">
+          <div class="card shadow">
+            <div class="card-header text-center">
+              <h1 class='fw-bold text-secondary'>User Profile</h1>
+            </div>
+            <div class="card-body p-5">
+                <table class='table table-striped table-bordered'>
+                    <tr>
+                        <th>Name</th>
+                        <td><?= $user['name'] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Email</th>
+                        <td><?= $user['email'] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Created At</th>
+                        <td><?= $user['created_at'] ?></td>
+                    </tr>
+                    <tr>
+                        <th>Updated At</th>
+                        <td><?= $user['updated_at'] ?></td>
+                    </tr>
+                </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
