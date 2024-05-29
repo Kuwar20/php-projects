2378468de26586e39494321dde2ca55308745712
Kuwar20 <kuwarx1@gmail.com>
2024-05-29 10:53:01 +0530

    Update register page with Bootstrap styling and form validation

--- secure_auth_system/register.php
+++ secure_auth_system/register.php
@@ -6,8 +6,44 @@
     <title>Register</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
-  <body>
-    <h1>Hello, world!</h1>
+  <body class='bg-dark bg-gradient'>
+    <div class="container">
+      <div class="row min-vh-100 justify-content-center align-items-center">
+        <div class="col-md-6">
+          <div class="card shadow">
+            <div class="card-header">
+              <h1 class='fw-bold text-secondary'>Register</h1>
+            </div>
+            <div class="card-body p-5">
+              <form action="action.php" method="POST">
+                <input type="hidden" name="register" value="1">
+                
+                <div class="mb-3">
+                  <label for="name">Name</label>
+                  <input type="text" name="name" id="name" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                  <label for="email">Email</label>
+                  <input type="email" name="email" id="email" class="form-control" required>
+                </div>
+                <div class="mb-3">
+                  <label for="password">Password</label>
+                  <input type="password" name="password" id="password" class="form-control" required>
+                </div>
+                <div class='mb-3'>
+                  <label for="confirm_password">Confirm Password</label>
+                  <input type="password" name="confirm_password" id="confirm_password" class="form-control" required>
+                </div>
+                <div class="mb-3 d-grid">
+                  <button type="submit" value="Register" class="btn btn-primary">Register</button>
+                </div>
+                <p class="text-center">Already have an account? <a href="/">Login</a></p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
