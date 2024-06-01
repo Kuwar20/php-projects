4dcea74cced06b09a93d09bd409b09896185aaa0
Kuwar20 <kuwarx1@gmail.com>
2024-06-01 10:28:03 +0530

    Add js for form submission

---
+++ crud-app/main.js
@@ -0,0 +1,27 @@
+const addForm = document.getElementById("add-user-form");
+
+// Add new User Ajax Request
+
+addForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(addForm);
+    formData.append("add", 1);
+
+    if(addForm.checkValidity() === false){
+        e.preventDefault();
+        e.stopPropagation();
+        addForm.classList.add("was-validated");
+        return false;
+    }else{
+        document.getElementById("add-user-btn").value='Please Wait...';
+
+        const data = await fetch('actions.php',{
+            method: 'POST',
+            body: formData
+        }).then(res => res.json());
+        
+        const response = await data.text();
+        console.log(response);
+
+    }
+});
