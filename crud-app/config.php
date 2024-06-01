ec35ef04a6b4a60ffa16033da5c63ec05f5a0c4f
Kuwar20 <kuwarx1@gmail.com>
2024-06-01 09:05:23 +0530

    chore: Update config.php with database connection details

---
+++ crud-app/config.php
@@ -0,0 +1,10 @@
+<?php
+
+        define('BASE_URL','http://localhost:80');
+
+        define('DB_HOST','localhost');
+        define('DB_PORT','3307');
+        define('DB_USER','root');
+        define('DB_PASS','');
+        define('DB_NAME','fetch_crud_app');
+?>
